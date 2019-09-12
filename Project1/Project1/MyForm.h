4ab33b23a0c57f02e2de27acd93878b11d19ef96
wilfredo morales <wmorales83@gmail.com>
2019-09-12 14:43:39 -0500

    ahora funciona

--- Project1/Project1/MyForm.h
+++ Project1/Project1/MyForm.h
@@ -16,6 +16,7 @@ namespace Project20 {
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 	private:
+		int cont = 0;
 	public:
 		MyForm(void)
 		{
@@ -39,6 +40,10 @@ namespace Project20 {
 	private: System::Windows::Forms::Button^  Mostrar_imagen;
 	protected:
 	private: System::Windows::Forms::Button^  Filtrar;
+	private: System::Windows::Forms::Timer^  timer1;
+	private: System::Windows::Forms::Label^  contador;
+	private: System::Windows::Forms::Label^  segss;
+	private: System::ComponentModel::IContainer^  components;
 			 //private: System::Windows::Forms::Button^  Filtrar;
 
 	protected:
@@ -47,7 +52,7 @@ namespace Project20 {
 		/// <summary>
 		/// Variable del diseñador necesaria.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -56,8 +61,12 @@ namespace Project20 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
 			this->Mostrar_imagen = (gcnew System::Windows::Forms::Button());
 			this->Filtrar = (gcnew System::Windows::Forms::Button());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->contador = (gcnew System::Windows::Forms::Label());
+			this->segss = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// Mostrar_imagen
@@ -80,18 +89,43 @@ namespace Project20 {
 			this->Filtrar->UseVisualStyleBackColor = true;
 			this->Filtrar->Click += gcnew System::EventHandler(this, &MyForm::Filtrar_Click);
 			// 
+			// timer1
+			// 
+			this->timer1->Interval = 1000;
+			this->timer1->Tick += gcnew System::EventHandler(this, &MyForm::timer);
+			// 
+			// contador
+			// 
+			this->contador->AutoSize = true;
+			this->contador->Location = System::Drawing::Point(420, 407);
+			this->contador->Name = L"contador";
+			this->contador->Size = System::Drawing::Size(26, 13);
+			this->contador->TabIndex = 2;
+			this->contador->Text = L"Seg";
+			// 
+			// segss
+			// 
+			this->segss->AutoSize = true;
+			this->segss->Location = System::Drawing::Point(482, 411);
+			this->segss->Name = L"segss";
+			this->segss->Size = System::Drawing::Size(13, 13);
+			this->segss->TabIndex = 3;
+			this->segss->Text = L"0";
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::White;
 			this->ClientSize = System::Drawing::Size(1096, 477);
+			this->Controls->Add(this->segss);
+			this->Controls->Add(this->contador);
 			this->Controls->Add(this->Filtrar);
 			this->Controls->Add(this->Mostrar_imagen);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
-			//ahdolor
+			this->PerformLayout();
 
 		}
 		int pivot(vector<int>* a, int i, int f) {
@@ -108,7 +142,7 @@ namespace Project20 {
 			}
 			if (der != i) {
 				int t = a->at(der);
-				a[der] = a[i];
+				a->at(der) = a->at(i);
 				a->at(i) = t;
 			}
 			return der;
@@ -124,15 +158,16 @@ namespace Project20 {
 			quick(a, 0, n - 1);
 		}
 		Color mediana(int x, int y, Bitmap^ ruido) {
+			x--; y--;
 			vector<int>* val = new vector<int>;
 			int cont = 0, quiero = 0;
-			for (int i = x - 1; i <= x + 1; i++) {
-				for (int e = y - 1; e <= y + 1; e++) {
+			for (int i = x; i <= x + 2; i++) {	  //	for (int i = x - 1; i <= x + 1; i++) {	
+				for (int e = y; e <= y + 2; e++) {// for (int e = y - 1; e <= y + 1; e++)
 					val->push_back(ruido->GetPixel(i, e).ToArgb());//explotaba aqui antes|
 					cont++;
 				}
 			}
-			quicksort(val, 9);
+			quicksort(val, val->size());
 			Color mediana = Color::FromArgb(val->at(4));
 			return mediana;
 		}
@@ -144,16 +179,23 @@ namespace Project20 {
 		graficador->DrawImage(ruido, Aimg);
 	}
 	private: System::Void Filtrar_Click(System::Object^  sender, System::EventArgs^  e) {
-
 		Graphics^ graficador = this->CreateGraphics();
-		Bitmap^ ruido = gcnew Bitmap("reduccion.jpg");
-		for (int i = 1; i < ruido->Height; i++) {//tal vez error con los pixeles que coge de la imagem(deberia ser hasta x-1 e y-1)
-			for (int e = 1; e < ruido->Width; e++) {
-				ruido->SetPixel(e, i, mediana(e, i, ruido));
+		Bitmap^ ruido = gcnew Bitmap("ruido.jpg");
+		this->timer1->Enabled = true;
+		for (char i = 0;i <= 2;i++) {
+			for (int i = 1; i < (ruido->Height - 1); i++) {//tal vez error con los pixeles que coge de la imagem(deberia ser hasta x-1 e y-1)
+				for (int e = 1; e < (ruido->Width - 1); e++) {
+					ruido->SetPixel(e, i, mediana(e, i, ruido));
+				}
 			}
 		}
-		Rectangle Aimg = Rectangle(60 + ruido->Width, 60 + ruido->Height, (ruido->Width*0.6), (ruido->Height*0.6));
+		Rectangle Aimg = Rectangle(60 + ruido->Width*0.6, 30, (ruido->Width*0.6), (ruido->Height*0.6));
 		graficador->DrawImage(ruido, Aimg);
+	}
+	private: System::Void timer(System::Object^  sender, System::EventArgs^  e) {
+		this->cont++;
+		this->segss->Text = cont.ToString();
+
 	}
 	};
 }
